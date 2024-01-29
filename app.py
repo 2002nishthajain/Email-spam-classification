a152ba897b7457a2d4c7590fa8339f62d246080b
2002nishthajain <2002nishthajain@gmail.com>
2024-01-29 16:42:26 +0530

    changes

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 import pickle
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
 app =  Flask(__name__)
 cv_path = os.path.join(script_dir, "models", "cv.pkl")
 clf_path = os.path.join(script_dir, "models", "clf.pkl")
+
+cv = pickle.load(open(cv_path, "rb"))
+clf = pickle.load(open(clf_path, "rb"))
+
 @app.route('/', methods=['GET','POST'])
 def home():
     text =""
@@ -25,6 +29,17 @@ def predict():
     else:
         # Handle the case where email is None (e.g., display an error message)
         return render_template("index.html", prediction=None, email=None)
+
+@app.route('/api/predict', methods=['POST'])
+def predict_api():
+    data = request.get_json(force=True)  # Get data posted as a json
+    email = data['content']
+    tokenized_email = cv.transform([email]) # X 
+    prediction = clf.predict(tokenized_email)
+    # If the email is spam prediction should be 1
+    prediction = 1 if prediction == 1 else -1
+    return jsonify({'prediction': prediction, 'email': email})  # Return prediction
+
 
 if __name__ == "__main__" :
     app.run(host="0.0.0.0", port=8080, debug=True)
